83b34c7fffaf6265e2f9745aaec601d0d26b0996
Dmitriy <dvol72@mail.ru>
2022-02-16 21:09:58 +0300

    changes

--- List2/List2.cpp
+++ List2/List2.cpp
@@ -31,16 +31,123 @@ class List
     }*Head, *Tail;
     size_t size;
 public:
+    class Iterator
+    {
+        Element* Temp;
+    public:
+        Iterator(Element* Temp = nullptr) : Temp(Temp)
+        {
+            cout << "ITConstructor:\t" << this << endl;
+        }
+
+        ~Iterator()
+        {
+            cout << "ItDestructor:\t" << this << endl;
+        }
+
+        Iterator& operator++()
+        {
+            Temp = Temp->pNext;
+            return *this;
+        }
+
+        Iterator& operator++(int)
+        {
+            Iterator old = *this;
+            Temp = Temp->pNext;
+            return old;
+        }
+
+        const int& operator *()const
+        {
+            return Temp->Data;
+        }
+        int& operator *()
+        {
+            return Temp->Data;
+        }
+
+        bool operator ==(const Iterator& other)const
+        {
+            return this->Temp == other.Temp;
+        }
+
+        bool operator !=(const Iterator& other)const
+        {
+            return this->Temp != other.Temp;
+        }
+
+        Iterator& operator--()
+        {
+            Temp = Temp->pPrev;
+            return *this;
+        }
+
+        Iterator& operator--(int)
+        {
+            Iterator old = *this;
+            Temp = Temp->pPrev;
+            return old;
+        }
+    };
+    Iterator begin()
+    {
+        return Head;
+    }
+    Iterator end()
+    {
+        return nullptr;
+    }
     List()
     {
         Head = Tail = nullptr;
         size = 0;
         cout << "LConstructor:\t" << this << endl;
     }
+    List(unsigned int size):List()
+    {
+        for (size_t i = 0; i < size; i++)
+        {
+            push_front(int());
+        }
+    }
+    List(const std::initializer_list<int>& il) :List()
+    {
+        for (int const* it = il.begin(); it != il.end() - 1; it++)
+        {
+            push_back(*it);
+        }
+    }
+
     ~List()
     {
+        while (Tail)
+        {
+            pop_back();
+        }
         cout << "LDestructor:\t" << this << endl;
     }
+
+    const int& operator[](int index)const
+    {
+        Element* Temp = Head;
+        for (size_t i = 0; i < index; i++)
+        {
+            Temp = Temp->pNext;
+        }
+        return Temp->Data;
+    }
+
+    int& operator[](int index)
+    {
+        Element* Temp = Head;
+        for (size_t i = 0; i < index; i++)
+        {
+            Temp = Temp->pNext;
+        }
+        return Temp->Data;
+    }
+
     void push_front(int Data)
     {
         if (Head==nullptr && Tail==nullptr)
@@ -66,48 +173,90 @@ public:
             return;
         }
         Element* New = new Element(Data);
-        Element* Temp = Head;
-        while (Temp->pNext)
-        {
-            Temp = Temp->pNext;
-        }
-        Temp->pNext = New;
+        New->pPrev = Tail;
+        Tail->pNext = New;
+        Tail = New;
         size++;
     }
 
     void pop_front()
     {
-        Element* Del = Head;
-        delete Del;
+        if (Head == nullptr && Tail == nullptr)return;
+        if (Head == Tail)
+        {
+            delete Head;
+            Head = Tail = nullptr;
+            size--;
+            return;
+        }
+        Head = Head->pNext;
+        delete Head->pPrev;
+        Head->pPrev = nullptr;
         size--;
+        
     }
 
     void pop_back()
     {
-        Element* Temp = Head;
-        while (Temp->pNext->pNext != nullptr)
-        {
-            Temp = Temp->pNext;
-        }
-        delete Temp->pNext;
-        Tail = Temp->pPrev;
-        Temp->pNext = nullptr;
+        if (Head == Tail)return pop_front();
+        Tail = Tail->pPrev;
+        delete Tail->pNext;
+        Tail->pNext = nullptr;
         size--;
+
     }
 
-    void insert(int Data, int index)
+    void insert(int index, int Data)
     {
-        Element* New = new Element(Data);
-        Element* Temp = Head;
-        for (size_t i = 0; i < index-1; i++)
+        if (index > size)return;
+        if (index == 0)return push_front(Data);
+        if (index == size)return push_back(Data);
+        Element* Temp;
+        if (index < size / 2)
         {
-            Temp = Temp->pNext;
+            Temp = Head;
+            for (size_t i = 0; i < index; i++)Temp = Temp->pNext;
         }
-        New->pNext = Temp->pNext;
-        Temp->pNext = New;
+        else
+        {
+            Temp = Tail;
+            for (size_t i = 0; i < size-index-1; i++)
+            {
+                Temp = Temp->pPrev;
+            }
+        }
+        Element* New = new Element(Data);
+        New->pNext = Temp;
+        New->pPrev = Temp->pPrev;
+        Temp->pPrev->pNext = New;
+        Temp->pPrev = New;
         size++;
     }
-    
+
+    void Erase(int index)
+    {
+        if (index > size)return;
+        if (index == 0)return pop_front();
+        if (index == size)return pop_back();
+        Element* Temp;
+        if (index < size / 2)
+        {
+            Temp = Head;
+            for (size_t i = 0; i < index; i++)Temp = Temp->pNext;
+        }
+        else
+        {
+            Temp = Tail;
+            for (size_t i = 0; i < size - index - 1; i++)
+            {
+                Temp = Temp->pPrev;
+            }
+        }
+        Temp->pPrev->pNext = Temp->pNext;
+        Temp->pNext->pPrev = Temp->pPrev;
+        delete Temp;
+        size--;
+    }
     
     void print()const
     {
@@ -127,8 +276,12 @@ public:
     }
 };
 
+//#define BASE_CHECK
+//#define CONSTR_CHECK
+
 int main()
 {
+#ifdef BASE_CHECK
     setlocale(LC_ALL, "rus");
     int n;
     cin >> n;
@@ -136,12 +289,29 @@ int main()
     for (size_t i = 0; i < n; i++)
     {
         //list.push_front(rand() % 100);
-        list.push_front(rand() % 100);
+        list.push_back(rand() % 100);
     }
     list.print();
-    list.insert(48, 1);
+    list.Erase(1);
     list.print();
+#endif // BASE_CHECK
+    
+
+#ifdef CONSTR_CHECK
+    List list = { 3, 5, 8, 13, 21 };
+    for (int i : list)cout << i << tab; cout << endl;
+#endif // CONSTR_CHECK
+
+    List list = { 3,5,8,13,21 };
+    list.print();
+    for (int i : list)
+    {
+        cout << i << tab;
+    }
+    cout << endl;
 }
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
